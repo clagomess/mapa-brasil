406871739df76423be80c04af8a32265c667cee8
Cláudio Gomes <cla.gomess@gmail.com>
2019-08-02 00:34:03 -0300

    wip - pontapé

--- src/mapa-brasil.js
+++ src/mapa-brasil.js
@@ -2,58 +2,69 @@ require('whatwg-fetch');
 
 'use strict';
 
-module.exports = async (element, options) => {
-  console.log("Disgraça");
-
-  let data = await loadDataFile(options);
-
-  console.log(data);
+module.exports = (element, options) => {
+  draw(element, options);
 };
 
-/*export default class {
-  /*draw = async (element, options) => {
-    // check
-    if (!options.hasOwnProperty('dataPath')) {
-      options.dataPath = 'data';
+let draw = (element, options) => {
+  options = validateOptions(options);
+
+  Promise.all([
+    loadDataFile(false, '/data/svg/low/federacao/br_unidades_da_federacao.svg'),
+    loadDataFile(true, 'data/json/federacao/br_unidades_da_federacao.json'),
+  ]).then(result => {
+    console.log(result);
+    element.innerHTML = result[0];
+
+    // SVG
+    let svgEl = element.getElementsByTagName('svg')[0];
+    svgEl.style.width = '100%';
+    svgEl.style.height = 'auto';
+
+    // paths
+    let listPath = svgEl.getElementsByTagName('g')[0].getElementsByTagName('path');
+    for (let path of listPath) {
+      path.style.fill = options.defaultFillColor;
+      path.style.stroke = options.defaultStrokeColor;
     }
+  });
+};
 
-    if (!options.hasOwnProperty('sigla')) {
-      options.sigla = 'br';
-    }
-
-    if (!options.hasOwnProperty('regiao')) {
-      options.regiao = 'federacao';
-    }
-
-    if (!options.hasOwnProperty('qualidade')) {
-      options.qualidade = 'low';
-    }
-
-    if (!options.hasOwnProperty('defaultFillColor')) {
-      options.defaultFillColor = '#FFF3E3';
-    }
-
-    if (!options.hasOwnProperty('defaultStrokeColor')) {
-      options.defaultStrokeColor = '#1F1A17';
-    }
-
-    let data = await loadDataFile(options);
-
-    console.log(data);
-  };
-
-  draw = () => {
-    console.log("uqlele");
+let validateOptions = (options) => {
+  // check
+  if (!options.hasOwnProperty('dataPath')) {
+    options.dataPath = '/data/';
   }
-}
-*/
 
-let loadDataFile = async (options) => {
-  //@TODO: implements options
-  fetch('/data/svg/low/federacao/br_unidades_da_federacao.svg').then((response) => {
-    console.log(response);
-    return response.text();
-  }).then((content) => {
-    console.log(content);
+  if (!options.hasOwnProperty('sigla')) {
+    options.sigla = 'br';
+  }
+
+  if (!options.hasOwnProperty('regiao')) {
+    options.regiao = 'federacao';
+  }
+
+  if (!options.hasOwnProperty('qualidade')) {
+    options.qualidade = 'low';
+  }
+
+  if (!options.hasOwnProperty('defaultFillColor')) {
+    options.defaultFillColor = '#FFF3E3';
+  }
+
+  if (!options.hasOwnProperty('defaultStrokeColor')) {
+    options.defaultStrokeColor = '#1F1A17';
+  }
+
+  return options;
+};
+
+let loadDataFile = async (isJson, path) => {
+  return new Promise((resolve, reject) => {
+    fetch(path).then((response) => {
+      return isJson ? response.json() : response.text();
+    }).then((content) => {
+      resolve(content);
+    }).catch(e => reject(e));
   });
 };
