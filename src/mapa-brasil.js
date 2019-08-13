37af9f9d3ffb20f7081f7902a9a243365b5f4555
CLAUDIO GOMES <claudio.silva@fnde.gov.br>
2019-08-13 16:57:10 -0300

    interactable: try fix issues

--- src/mapa-brasil.js
+++ src/mapa-brasil.js
@@ -10,8 +10,55 @@ const interactable = require('./core/interactable');
 
 'use strict';
 
-module.exports = (element, options) => {
-  draw(element, options);
+let closeLoader = (element) => {
+  const loader = element.getElementsByClassName('loader')[0];
+  const svgContainer = element.getElementsByClassName('svg-container')[0];
+
+  loader.style.display = 'none';
+  svgContainer.style.display = 'block';
+};
+
+let createCssHeader = () => {
+  if(document.getElementById('mapa-brasil-css')){
+    return;
+  }
+
+  let style = document.createElement('style');
+  style.setAttribute("type", "text/css");
+  style.setAttribute("id", "mapa-brasil-css");
+  style.innerHTML = constantes.css;
+
+  document.head.appendChild(style);
+};
+
+let getPath = function(options, isJson) {
+  let path = options.dataPath;
+
+  if(isJson) {
+    path += `/json/${constantes.mapPath[options.regiao]}/`;
+  }else{
+    path += `/svg/${options.qualidade}/${constantes.mapPath[options.regiao]}/`;
+  }
+
+  if(options.regiao === 'federacao'){
+    path += 'br_unidades_da_federacao';
+  }else {
+    path += options.unidade + '_' + constantes.mapPath[options.regiao];
+  }
+
+  path += (isJson ? '.json' : '.svg');
+
+  return path;
+};
+
+let loadDataFile = async (isJson, path) => {
+  return new Promise((resolve, reject) => {
+    fetch(path).then((response) => {
+      return isJson ? response.json() : response.text();
+    }).then((content) => {
+      resolve(content);
+    }).catch(e => reject(e));
+  });
 };
 
 let draw = (element, options) => {
@@ -74,53 +121,6 @@ let draw = (element, options) => {
   });
 };
 
-let closeLoader = (element) => {
-  const loader = element.getElementsByClassName('loader')[0];
-  const svgContainer = element.getElementsByClassName('svg-container')[0];
-
-  loader.style.display = 'none';
-  svgContainer.style.display = 'block';
-};
-
-let createCssHeader = () => {
-  if(document.getElementById('mapa-brasil-css')){
-    return;
-  }
-
-  let style = document.createElement('style');
-  style.setAttribute("type", "text/css");
-  style.setAttribute("id", "mapa-brasil-css");
-  style.innerHTML = constantes.css;
-
-  document.head.appendChild(style);
-};
-
-let getPath = function(options, isJson) {
-  let path = options.dataPath;
-
-  if(isJson) {
-    path += `/json/${constantes.mapPath[options.regiao]}/`;
-  }else{
-    path += `/svg/${options.qualidade}/${constantes.mapPath[options.regiao]}/`;
-  }
-
-  if(options.regiao === 'federacao'){
-    path += 'br_unidades_da_federacao';
-  }else {
-    path += options.unidade + '_' + constantes.mapPath[options.regiao];
-  }
-
-  path += (isJson ? '.json' : '.svg');
-
-  return path;
-};
-
-let loadDataFile = async (isJson, path) => {
-  return new Promise((resolve, reject) => {
-    fetch(path).then((response) => {
-      return isJson ? response.json() : response.text();
-    }).then((content) => {
-      resolve(content);
-    }).catch(e => reject(e));
-  });
+module.exports = (element, options) => {
+  draw(element, options);
 };
